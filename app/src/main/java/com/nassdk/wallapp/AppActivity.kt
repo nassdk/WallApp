639935769a15a6d39bea187e2b6af48605cd740d
nassdk <nassdk17@gmail.com>
2021-05-24 02:14:49 +0300

    Hilt fixes.

--- app/src/main/java/com/nassdk/wallapp/AppActivity.kt
+++ app/src/main/java/com/nassdk/wallapp/AppActivity.kt
@@ -2,10 +2,24 @@ package com.nassdk.wallapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.nassdk.wallapp.feature.newsfeed.presentation.NewsFeedFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AppActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.add(
+                R.id.app_container,
+                NewsFeedFragment(),
+                "NewsFeedFragment"
+            )
+            transaction.commit()
+        }
     }
 }
--- app/src/main/java/com/nassdk/wallapp/library/coreimpl/common/resourcemanager/ResourceManagerImpl.kt
+++ app/src/main/java/com/nassdk/wallapp/library/coreimpl/common/resourcemanager/ResourceManagerImpl.kt
@@ -2,10 +2,11 @@ package com.nassdk.wallapp.library.coreimpl.common.resourcemanager
 
 import android.content.Context
 import com.nassdk.wallapp.library.coreapi.common.resourcemanager.ResourceManager
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class ResourceManagerImpl @Inject constructor(
-    private val context: Context
+    @ApplicationContext private val context: Context
 ) : ResourceManager {
     override fun getString(resId: Int) = context.getString(resId)
     override fun getString(resId: Int, vararg formatArg: Any) = context.getString(resId, formatArg)
